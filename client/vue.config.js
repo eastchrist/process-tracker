dd7e1feae26f3a8c0a4a6a2984162c543b6e7e9c
eastchrist <east_christ@hotmail.com>
2021-07-04 03:56:19 +0300

    Update vue.config.js

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
+
 //const outputDirectory = path.resolve(__dirname, 'src', 'main', 'resources', 'public');
 //const contentBaseDir = path.resolve(__dirname, 'src', 'main', 'resources', 'assets');
 
