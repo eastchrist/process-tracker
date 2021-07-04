ab51e9553c4e4aa85aa51109edb89232c19b88ee
eastchrist <east_christ@hotmail.com>
2021-07-04 03:37:49 +0300

    Create projectStatus.ts

---
+++ client/src/api/projectStatus.ts
@@ -0,0 +1,60 @@
+import requestSql from '@/utils/requestSql'
+import { IDataBaseProjectStatusData } from './types'
+import i18n from "@/i18n";
+import variables from "@/styles/_variables.scss";
+
+
+export interface AIProjectStatusQuery {
+    page: number
+    limit: number
+    name: string | undefined
+}
+
+export const defaultProjectStatusQuery: AIProjectStatusQuery = {
+    page: 1,
+    limit: 10,
+    name: undefined
+}
+
+export const defaultProjectStatusData: IDataBaseProjectStatusData = {
+    id: "",
+    position: 0,
+    name: "",
+    value: 0
+}
+
+export const defaultProjectStatusExcelHeater = ['id', 'position', 'name', 'value']
+
+export const getDBProjectStatus = (params: any) =>
+    requestSql({
+        headers: { },
+        url: '/projectStatus/all',
+        method: 'get',
+        params
+    })
+
+export const updateDBProjectStatuss = (data: any) =>
+    requestSql({
+        headers: { },
+        url: `/projectStatus/all`,
+        method: 'put',
+        withCredentials: true,
+        data
+    })
+
+export const updateDBProjectStatus = (id: number, data: any) =>
+    requestSql({
+        headers: { },
+        url: `/projectStatus/projectStatus/${id}`,
+        method: 'put',
+        withCredentials: true,
+        data
+    })
+
+export const deleteDBProjectStatus = (id: number, data: any) =>
+    requestSql({
+        headers: { },
+        url: `/projectStatus/projectStatus/${id}`,
+        method: 'delete',
+        withCredentials: true,
+    })
